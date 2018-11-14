888df9b51697ddc1e58746d0e92ce926610e87eb
IanJWilson <ianjwilson8@outlook.com>
2018-11-14 13:50:13 -0500

    new scripts

---
+++ Assets/Ian's Assets/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float Enemyhealth;
+    public float Enemyspeed;
+    public float Enemydamage;
+
+
+    public float MinDis;
+    public float MaxDis;
+
+    public Transform Player;
+	// Use this for initialization
+	void Start ()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Enemyhealth--;
+
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDis)
+        {
+
+            transform.position += transform.forward * Enemyspeed * Time.deltaTime;
+
+
+
+           if (Vector3.Distance(transform.position, Player.position) <= MaxDis)
+            {
+                transform.position = transform.position;
+            }
+
+        }
+
+
+
+
+
+
+
+
+        if (Enemyhealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+		
+	}
+}
