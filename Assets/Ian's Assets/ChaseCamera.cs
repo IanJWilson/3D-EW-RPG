d447296b64bbf8d09921402adebda3bc2da25a78
SacroSanctus <44281233+SacroSanctus@users.noreply.github.com>
2018-12-05 14:50:36 -0500

    UpdtPlyr

---
+++ Assets/Ian's Assets/ChaseCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseCamera : MonoBehaviour
+{
+
+    public Transform wheel;
+    public GameObject player;
+    public float distance;
+    public float height;
+    public float rotationDamping = 3f;
+    public float heightDamping = 2f;
+    private float desiredAngle = 0;
+
+
+    // LateUpdate is called once per frame after Update() has been called
+    void LateUpdate()
+    {
+        if (player.GetComponent<PlayerWheel>().isAttacking == false)
+        {
+            float currentAngle = transform.eulerAngles.y;
+            float currentHeight = transform.position.y;
+            //Determine where we want to be.
+            desiredAngle = wheel.eulerAngles.y;
+            float desiredHeight = wheel.position.y + height;
+            //Now move towards our goals.
+            currentAngle = Mathf.LerpAngle(currentAngle, desiredAngle, rotationDamping * Time.deltaTime);
+            currentHeight = Mathf.Lerp(currentHeight, desiredHeight, heightDamping * Time.deltaTime);
+            Quaternion currentRotation = Quaternion.Euler(0, currentAngle, 0);
+            //Set our new positions.
+            Vector3 finalPosition = wheel.position - (currentRotation * Vector3.forward * distance);
+            finalPosition.y = currentHeight;
+            transform.position = finalPosition;
+            transform.LookAt(wheel);
+        }
+    }
+}
--- Assets/Mike's Assets/Scripts/PlayerWheel.cs
+++ Assets/Mike's Assets/Scripts/PlayerWheel.cs
@@ -14,16 +14,21 @@ public class PlayerWheel : CharacterScript {
     public Transform wheelTransformFL;
     public Transform wheelTransformFR;
 
+    public GameObject SpearNShield;
     public GameObject SpearL;
     public GameObject ShieldR;
 
+    public GameObject Swords;
     public GameObject SwordL;
     public GameObject SwordR;
 
     //public GameObject SpearR;
 
+    public bool isAttacking;
+    public bool spearEq;
+    public bool swordEq;
 
-        
+
     public float currentSpeed = 0;
     public float topSpeed = 150;
     public float maxReverseSpeed = -50;
@@ -31,12 +36,12 @@ public class PlayerWheel : CharacterScript {
     public float maxTorque = 10;
     public Vector3 centerOfMassAdjustment = new Vector3(0f, -0.9f, 0f);
 
+    public float groundDist = 0f;
 
 
 
-    
     // Use this for initialization
-    void Start () {
+    void Start() {
         //lower center of mass for roll-over resistance
         body = GetComponent<Rigidbody>();
         body.centerOfMass += centerOfMassAdjustment;
@@ -46,11 +51,53 @@ public class PlayerWheel : CharacterScript {
     // Update is called once per frame
     void Update()
     {
+        
+        groundDist = GroundRayCast();
         //rotate the wheels based on RPM
-        float rotationThisFrame = 180 * Time.deltaTime;
+        float rotationThisFrame = 360 * Time.deltaTime;
         wheelTransformFR.Rotate(wheelBR.rpm / rotationThisFrame, 0, 0);
         SwordSwing(SwordL, SwordR);
-        //UpdateWheelPositions();
+
+        if (Input.GetKeyDown("1"))
+        {
+            SpearNShield.SetActive(true);
+            Swords.SetActive(false);
+            spearEq = true;
+            swordEq = false;
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            SpearNShield.SetActive(false);
+            Swords.SetActive(true);
+            spearEq = false;
+            swordEq = true;
+        }
+
+
+
+        if (swordEq) { 
+            if (Input.GetButton("Fire1") && Mathf.Abs(GroundRayCast()) > 0f)
+            {
+                isAttacking = true;
+                body.constraints = RigidbodyConstraints.FreezePosition;
+                body.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+                transform.RotateAround(transform.position, Vector3.up, 1080.0f * Time.deltaTime / 1f);
+
+
+            }
+        if (Input.GetButtonUp("Fire1") && Mathf.Abs(GroundRayCast()) > 0f)
+        {
+                isAttacking = false;
+            body.constraints = RigidbodyConstraints.FreezePosition;
+            body.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
+        }
+    }
+
+        if(spearEq){
+
+        }
+
     }
 
     void FixedUpdate()
@@ -73,7 +120,7 @@ public class PlayerWheel : CharacterScript {
         //front wheel steering
         wheelFL.steerAngle = Input.GetAxis("Horizontal") * maxTurnAngle;
         wheelFR.steerAngle = Input.GetAxis("Horizontal") * maxTurnAngle;
-
+        
     }
 
     void UpdateWheelPositions()
@@ -101,5 +148,33 @@ public class PlayerWheel : CharacterScript {
             wheelTransformBR.position = temp;
         }
     }
+
+    public float GroundRayCast()
+    {
+        RaycastHit hit;
+        Vector3 wheelCenter = transform.position + (transform.forward * .5f);
+        Debug.DrawRay(wheelCenter, transform.up * -.4f );
+        //if we detect a car infront of us, slow down or even reverse based on distance.
+        if ((Physics.Raycast(wheelCenter, transform.up, out hit, -.4f))==false)
+        {
+            return (((wheelCenter - hit.point).magnitude / .4f));
+        }
+        //otherwise no change
+         return 0f;
+    }
+
+    //IEnumerator Rotate(float duration)
+    //{
+    //    float startRotation = transform.eulerAngles.y;
+    //    float endRotation = startRotation + 360.0f;
+    //    float t = 0.0f;
+    //    while (t < duration)
+    //    {
+    //        t += Time.deltaTime;
+    //        float yRotation = Mathf.Lerp(startRotation, endRotation, t / duration) % 360.0f;
+    //        transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+    //        yield return null;
+    //    }
+    //}
 
 }
